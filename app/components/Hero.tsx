3d14dd79c9c0f61f8cfdb02d6f8ad211e093da93
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-16 17:17:02 +0200

    essaie hero avec color en dur

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,32 +1,25 @@
 "use client";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
     return (
-        <section className="relative h-[80vh] w-full overflow-hidden">
-        <Image
-            src="/images/hero-nyc-taxi.jpg"
-            alt="Taxi jaune à New York"
-            fill
-            priority
-            className="object-cover object-bottom"
-        />
-
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
-            <h1 className="text-5xl md:text-6xl font-serif font-bold uppercase tracking-wider">
-            Code in the City
-            </h1>
-            <p className="mt-4 text-xl italic font-light">
-            Séries, Sounds & Stories
-            </p>
-            <Link
-            href="/blog"
-            className="mt-8 px-6 py-3 bg-white text-black uppercase font-semibold tracking-wide rounded hover:bg-gray-200 transition"
-            >
-            Entrer
-            </Link>
-        </div>
+        <section className="relative h-[60vh] md:h-[70vh] w-full overflow-hidden">
+            <Image
+                src="/images/hero-nyc-taxi.jpg"
+                alt="Taxi à New York sur le pont de Brooklyn"
+                fill
+                className="object-cover object-bottom"
+                priority
+            />
+            <div className="absolute inset-0 bg-black/70 flex items-end justify-center pb-12 px-4">
+                <Link
+                    href="/blog"
+                    className="uppercase tracking-wider px-6 py-2 bg-[#f1cd52] !text-black font-semibold rounded shadow-md ring-1 ring-black/20 hover:brightness-110 transition"
+                    >
+                    Entrer
+                </Link>
+            </div>
         </section>
     );
 }
