314d9add03186d7d3ce9b4d387b957b163056c99
Gaëlle <gaelleboucher.dev@gmail.com>
2025-04-26 00:07:43 +0200

    style series

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -12,24 +12,25 @@ export default function SeriesPage() {
         Toutes les séries
       </h1>
 
-      <section className="grid md:grid-cols-2 gap-8">
+      <section className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 py-12">
         {series.map((serie: Serie) => (
-          <div
-            key={serie.id}
-            className="border p-6 rounded shadow-md bg-white hover:shadow-lg transition"
-          >
-            <h2 className="text-2xl font-bold font-serif mb-2">{serie.title}</h2>
-            <p className="text-gray-700 mb-4 italic">{serie.years}</p>
-            <p className="text-gray-800">{serie.synopsis}</p>
-            <Link
-              href={`#`}
-              className="mt-4 inline-block text-blue-600 hover:underline"
-            >
-              Lire la fiche
-            </Link>
-          </div>
-        ))}
-      </section>
+      <div
+        key={serie.id}
+        className="border-2 border-[var(--color-secondary)] rounded shadow-md bg-white hover:shadow-lg transition hover:scale-[1.01] hover:-translate-y-1 duration-300 ease-in-out p-6">
+        <div>
+          <h2 className="text-2xl font-serif font-bold mb-2 text-gray-900">{serie.title}</h2>
+          <p className="text-sm text-gray-500 italic mb-2">{serie.years}</p>
+          <p className="text-gray-700 mb-4">{serie.synopsis}</p>
+        </div>
+        <Link
+          href="#"
+          className="text-blue-600 font-medium hover:underline mt-auto self-start"
+        >
+          Lire la fiche →
+        </Link>
+      </div>
+    ))}
+    </section>
     </main>
   );
 }
