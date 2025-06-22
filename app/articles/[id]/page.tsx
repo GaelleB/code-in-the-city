fa70d6022e2258c2a19c4d3fb7e82fe555cd1fb6
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-06-22 23:27:11 +0200

    ajout padding article

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,3 +1,4 @@
+// app/articles/[id]/page.tsx
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { articles } from "@/data/articles";
@@ -7,16 +8,17 @@ export default function ArticlePage({ params }: { params: { id: string } }) {
   if (!article) return notFound();
 
   return (
-    <article className="prose mx-auto px-4 py-12 text-[var(--color-dark)]">
-      <h1>{article.title}</h1>
-      <p className="text-sm text-gray-600">{article.date}</p>
+    <article className="mx-auto px-4 sm:px-6 lg:px-20 py-12 text-[var(--color-dark)]">
+      <h1 className="text-xl text-[var(--color-dark)] mb-4">{article.title}</h1>
+      <p className="text-sm text-[var(--color-dark)] mb-4">{article.date}</p>
       <Image
         src={article.image}
         alt={article.title}
-        width={400}
-        height={200}
+        width={600}
+        height={400}
         className="rounded-lg my-6"
       />
+
       {article.content.map((paragraph, idx) => (
         <p key={idx} className="whitespace-pre-line">
           {paragraph}
