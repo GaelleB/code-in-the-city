4b7fdd78e5fe44551c4300c8080e0ca62951800b
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-06-23 10:10:35 +0200

    remove compo categorycolumn

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/Hero";
 import Featured from "@/components/Featured";
-import CategoryColumns from "@/components/CategoryColumns";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <Featured />
-      <CategoryColumns />
     </main>
   );
 }
